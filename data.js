27e9760d45b8f3070fb3266e5a8583c920b93acc
celalcok <celalcok@gmail.com>
2022-09-08 23:00:08 +0300

    some data added data.js

--- data.js
+++ data.js
@@ -41,4 +41,173 @@ export const texts = [
         text:"Prunus armeniaca 8-12 metre (26-39 ft) 40 santimetre (16 in) kadar gövde ile çapında ve yoğun, yayılan bir kanopi. Yapraklar oval, 5-9 santimetre (2,0-3,5 in) uzun ve 4-8 santimetre (1,6-3,1 in) genişliğinde, yuvarlak tabanlı, sivri uçlu ve ince tırtıklı kenarlı küçük bir ağaçtır. Çiçekler 2-45 santimetre (0,8-17,7 in) çapında, beş beyaz ila pembemsi yaprakları ile; İlkbaharda yapraklardan önce tek başlarına veya çiftler halinde üretilirler. Küçük bir şeftaliye benzer bir sert çekirdekli 15-25 santimetre (5,9-9,8 in) çapında (bazı modern çeşitlerde daha büyük) meyveleri (drupa) vardır. sarıdan turuncuya, genellikle güneşe en çok maruz kalan tarafta kırmızı renkte; yüzeyi pürüzsüz (botanik olarak tüysüz olarak tanımlanır) veya kadifemsi ve çok kısa tüylü (botanik olarak: tüylü) olabilir. Meyve eti genellikle serttir ve çok sulu değildir. Tadı tatlıdan ekşiye kadar değişebilir. Tek tohum, bir taraftan aşağı doğru inen üç sırt dışında, grenli, pürüzsüz bir dokuya sahip, genellikle 'taş' olarak adlandırılan sert, taşlı bir kabuğun içine yerleştirilmiştir ",
         res:"https://tr.wikipedia.org/wiki/Kayısı"
     },
+    {
+        id:7,
+        lang:"tr",
+        title:"Düşünce Gücüyle Sorun Çözme",
+        text:"Yüce Yaratıcının evrende işleyen bir kanunu var. O kanuna göre; sebeplere tabi olmak gerek. Ancak bazen sebepler felç edilir ve Evrenin Sahibi’nin planı işler, insan olarak birçok konuda acizliğimizi fark etmek zorundayız. Kişisel gelişim diyerek, insanların egosunun şişirilmesine ve bencil ruhların ortaya çıkmasına sebep olunuyor. Tabiî ki gelişeceğiz. Düşünce tekniklerini, yönetim tekniklerini, iletişim tekniklerini öğrenecek ve uygulayacağız. Ancak bunları uygularken O’na sığınacak ve yardım dileyeceğiz. Samimi olacağız. Yüreklere hitap edeceğiz. Kendi menfaatlerimizi bir kenara bırakacak, insana insan olduğu için saygı duyacağız. “Ben başarırım, ben yaparım, sınırsız güç, içinizdeki dev” gibi söylemler risklidir. Gururlanmak, insanın kendini üstün görmesi aklın kıtlığına işarettir. Ateş düştüğü yeri yakar; bencillik, ateş çevresini de yakar; bir parça fedakârlık, ateş beni de yakar ise olgunluk mertebesidir. Üçüncü düşüncenin temsilcisi olmaya gayret edelim…",
+        res:"https://onlinekitapoku.com/dusunce-gucuyle-sorun-cozme-olumlu-ve-sistemli-dusunce-sorun-cozmenin-anahtaridir/"
+    },
+    {
+        id:8,
+        lang:"tr",
+        title:"Tarihe Geçen Hazır Cevaplar",
+        text:"Einstein şoförüyle birlikte planını uygulamaya koyar. Şoför gerçekten de başarılı bir konuşma yapar. Konferansın sonunda sorular sorulur ve şoför hepsini doğru şekilde yanıtlar. Tam yerine oturacağı esnada son soru gelir. Bugüne dek konferanslarda sorulmayan bir sorudur bu. Ancak şoför kıvrak zekâsını kullanarak “Bu konferansın ardından bu derece basit bir soru sormanızı yadırgadım” der, “Ancak her sorunun yine de cevaplanması gerekir”. Bu sözlerin sonrasında arka sırada bulunan Einstein’ı işaret ederek “Şimdi size sorunun basitliğini de ispatlayacağım, soru o kadar basit ki buna şoförüm bile yanıt verebilir” der.",
+        res:"https://onlinekitapoku.com/tarihe-gecen-hazircevaplar/"
+    },
+    {
+        id:9,
+        lang:"tr",
+        title:"Perili Ev",
+        text:"Karşımdaki adam bütün gece harıl harıl, hiç bitmeyecekmiş kadar işle uğraşmıştı. Bu mantıksız davranışına ek olarak (ondan da sadece bu beklenirdi) elinde bir not defteriyle bir kalem, sürekli bir şeylere kulak kabartıyor ve notlar alıyordu. Önceleri bu sinir bozucu notların trenin sarsıntılarıyla ilgili olduğunu düşündüm ve eğer bu seslere her kulak kabarttığında gözlerini başımın üzerine dikmeseydi kendisinin de bir inşaat mühendisi olduğunu varsayarak onu hoş görebilirdim. Şaşı, yüzünde şaşkın bir ifade olan biriydi ve davranışları giderek tahammül edilemez bir hal almıştı.",
+        res:"https://onlinekitapoku.com/perili-ev/"
+    },
+    {
+        id:10,
+        lang:"tr",
+        title:"Şehir Faresi ve Tarla Faresi",
+        text:"Şehir faresi arkadaşı tarla faresini her zaman tarlaya giderek ziyaret edermiş. Onu konuk olup her gün beraberce yemeklerini yerlermiş. Bir gün şehir faresi, arkadaşı tarla faresini yaşadığı şehre davet etmiş. Şehir faresi o kadar çok ısrar etmiş ki sonunda şehir hayatını pek sevmeyen yoksul tarla faresi; “Davete icabet etmek gerekir, gidip şu arkadaşımın evini ziyaret edeyim bari” diyerek yola koyulmuş. Tarla faresi yolculuğunu tamamlayıp da şehir faresinin yaşadığı eve ulaşınca gözlerine inanamamış. “Aman tanrım bu ne büyük bir zenginlik böyle.”diyerek hayretini gizlememiş.İçeri girince daha bir etkilenmiş gördüklerinden,hele o ziyafet sofrasını görünce dili tutulmuş. Bir tek kuş sütü eksikmiş sofrada.Güzel yemekler çeşit çeşit peynirler ve akla gelebilecek her yiyecek onları bekliyormuş.",
+        res:"http://www.masaloku.com/sehir-faresi-ve-tarla-faresi.htm"
+    },
+    {
+        id:11,
+        lang:"tr",
+        title:"Altın Yumurtlayan Tavuk",
+        text:"Bir varmış, bir yokmuş. Bir zamanlar, sevimli, küçücük bir köyde yoksul bir köylü yaşarmış. Bu köylünün bir tavuğu varmış. Köylü bu tavuğu çok severmiş. Tavuk da ona her gün bir yumurta yaparmış. Ama bu yumurtaların ilginç bir özelliği varmış. Yumurtalar altındanmış. Köylü her gün kümesten aldığı altın yumurtayı şehre götürür, kuyumcuda satarmış.Yoksul köylü giderek zenginleşmeye başlamış. Zenginleştikçe huyu değişiyormuş. Artık para kazanmak için çalışmak zorunda kalmıyormuş. Çalışmadan, yorulmadan para geldiği içinde paranın değerini bilmiyormuş. Gereksiz yere para harcamaya, ihtiyacı olmayan şeyler almaya başlamış. Lüks içinde yaşamaya alıştığından bir süre sonra para yetersiz gelmeye başlamış.",
+        res:"http://www.masaloku.com/altin-yumurtlayan-tavuk.htm"
+    },
+    {
+        id:11,
+        lang:"tr",
+        title:" Çınar Ağacı ile Saz",
+        text:"Kocaman bir çınar ağacıyla incecik gülden narin bir saz birbirlerine komşuymuş. Çınar saza tepeden bakarak şöyle demiş: “ bir kendime bir sana bakıyorum da acıyorum sana ne kadar ince ne kadar narin canlılarsınız böyle. En küçük rüzgâr da hemen beliniz bükülür. En küçük su dalgası anında ürpertir. Zavallılar.” Saz içini çekerek, “haklısın” demiş. Çınar iyice küçümsemiş onu. “bir de bana bak. Ne kadar haşmetliyim, güçlü kuvvetliyim. Gövdem senin gövdenin neredeyse bin katı. Dallarımın sıklığından güneş kollarını toprağa ulaştıramıyor. Kuşların çokluğundan dallarım neredeyse görünmeyecek. Rüzgâr vız gelir bana… Dilersen sen de gel benim gölgeme sığın birlikte yaşayalım. Saz çınarın sözlerini gülerek karşılamış. “ ben inceyim ama rüzgârdan büküldüğüm de kırılmam. Çünkü gövdem esnektir benim ama sen… Der demez bir rüzgâr bir fırtına ortalığı birbirine katmış. Rüzgâr zavallı ulu çınarı kökünden söküp atmış.",
+        res:"http://www.masaloku.com/cinar-agaci-ile-saz.htm"
+    },
+    {
+        id:12,
+        lang:"tr",
+        title:"Kırmızı Başlıklı Kız",
+        text:"Çok eski zamanların birinde küçücük bir kız varmış. Küçük kızın annesi ona kırmızı başlıklı bir pelerin almış. Küçük kız bu kırmızı başlıklı pelerini çok ama çok seviyormuş ve üzerinden hiç çıkarmıyormuş. Küçük kızın bu davranışı nedeniyle tüm çevresi ona Kırmızı Başlıklı Kız diyormuş. Bir gün annesi ona seslenmiş: “Büyükannen hâlâ hasta. Hadi giyin de, ona yaptığım şu çöreği götür.” Bunun üzerine Kırmızı Başlıklı Kız üzerini giyinerek eline çöreği almış ve yola koyulmuş. Annesi küçük kızına “Tavşan Ormanı’ndaki yoldan ayrılma sakın!” diye seslenmiş. Küçük kız da “Ayrılmam anne,” demiş. Kırmızı Başlıklı Kız ormana girip yürümeye başladığında çalılıkların arasından gelen bir ses duymuş. Bu sesin ardından birdenbire yola bir kurt çıkıvermiş. Küçük kız çok ama çok korkmuş. Fakat kurt, küçük kıza hiç de düşmanca bakmıyormuş. “Nereye böyle küçük kız?” diye sormuş kurt. Büyükanneme gidiyorum, demiş Kırmızı Başlıklı Kız. “Tavşan Ormanı’nın sonundaki ilk ev. (Ormanın adını Tavşan Ormanı’ymış). Büyükannem çok hasta. Kurt kıza seslenmiş: “Bak sana ne diyeceğim. Ben hızlıca gidip büyükanneme senin ziyaretine geldiğini söyleyeyim. Sen de yolda çok eğleşmeden hemen gel.” Kurt, hemen oradan ayrılmış, çünkü ormandaki oduncunun onu bulma ihtimali varmış. Eğer kızı hemen orada yemeye çalışırsa oduncunun onu bulmasından korkuyordu.",
+        res:"http://www.masaloku.com/kirmizi-baslikli-kiz-masali.htm"
+    },
+    {
+        id:13,
+        lang:"tr",
+        title:"Bremen Mızıkacıları",
+        text:"Eski zamanlardan birinde bir adam ve onun bir eşeği varmış. Bu eşek yorgunluk nedir bilmeden çuvalların hepsini taşırmış. Fakat gel zaman git zaman gücünden kaybetmeye ve artık çuvalları taşıyamamaya başlamış. Eşeğin sahibi ise eşeği işe yaramaz olarak görmeye başlamı ve onu boş yere beslemeyi gereksiz görüyormuş. Eşek, bu durumun farkına varıp sahibini daha fazla rahatsız etmek istemeği için başını alıp çıkmış ve Bremen şehrinin yolunu tutmuş. Yük taşıyamıyorsa Bremen’de çalgıcı olabileceğini düşünmüş.Eşek yolda yürürken bir köpek ile karşılaşmış Köpek av köpeğiydi ve yolda upuzun yatıyormuş. Köpek o kadar yorgun bir şekilde yatıyormuş ki, burnundan soluyormuş.",
+        res:"http://www.masaloku.com/bremen-mizikacilari-masali.htm"
+    },
+    {
+        id:14,
+        lang:"tr",
+        title:" Aslan ve Tavşan",
+        text:"Çok eski zamanların birinde ormanların birisinde çok zalim bir aslan yaşarmış ve kimi görse bu zalimliği ile korkuturmuş. Ormanda yaşayan tüm hayvanlar zalim aslanın bu halinden aşırı şikayetçilermiş. Aradan zaman geçtikçe artık bir çare bulmaları gerektiğinizi düşünmüşler ve hep beraber toplanıp bu konuyla ilgili bir kara almışlar. Aldıkları bu kesin kararı aslana söylemek için hep beraber yola koyulmuşlar.Sayın pek değerli yüce mi yüce efendimiz, biz orman sakinleri olarak aldığımız bir kararı size bildirmek için geldik. Artık siz hiç yorulmadan biz kendi aramızda siz acıktığınız zaman kura ile birisini seçip size sunacağız.Ormanların efendisi zalim aslan orman sakinlerinin aldığı bu karardan çok mutlu olur. Bu kadar sevinmesinin sebebi ise artık avlanmaya çıkmasına gerek kalmamasıydı. Bu olaydan sonra her gün kuradan kim çıktıysa gelip kendini aslana sundu ve aslanın o gün için yemeği oldu.",
+        res:"http://www.masaloku.com/aslan-ve-tavsan.htm"
+    },
+    {
+        id:15,
+        lang:"en",
+        title:"The Father",
+        text:`THE man whose story is here to be told was the wealthiest and most influential person in his parish; his name was Thord Overaas. He appeared in the priest's study one day, tall and earnest. "I have gotten a son," said he, "and I wish to present him for baptism."
+
+        "What shall his name be?"
+        
+        "Finn,—after my father."
+        
+        "And the sponsors?"
+        
+        They were mentioned, and proved to be the best men and women of Thord's relations in the parish.
+        
+        "Is there anything else?" inquired the priest, and looked up.
+        
+        The peasant hesitated a little.
+        
+        "I should like very much to have him baptized by himself," said he, finally.
+        
+        "That is to say on a week-day?"
+        
+        "Next Saturday, at twelve o'clock noon."
+        
+        "Is there anything else?" inquired the priest.
+        
+        "There is nothing else;" and the peasant twirled his cap, as though he were about to go.
+        
+        Then the priest rose. "There is yet this, however," said he, and walking toward Thord, he took him by the hand and looked gravely into his eyes: "God grant that the child may become a blessing to you!"
+        
+        One day sixteen years later, Thord stood once more in the priest's study.`,
+        res:"https://americanliterature.com/author/bjrnstjerne-bjrnson/short-story/the-father"
+    },
+    {
+        id:16,
+        lang:"en",
+        title:"The Terrible Old Man",
+        text:`The inhabitants of Kingsport say and think many things about the Terrible Old Man which generally keep him safe from the attention of gentlemen like Mr. Ricci and his colleagues, despite the almost certain fact that he hides a fortune of indefinite magnitude somewhere about his musty and venerable abode. He is, in truth, a very strange person, believed to have been a captain of East India clipper ships in his day; so old that no one can remember when he was young, and so taciturn that few know his real name. Among the gnarled trees in the front yard of his aged and neglected place he maintains a strange collection of large stones, oddly grouped and painted so that they resemble the idols in some obscure Eastern temple. This collection frightens away most of the small boys who love to taunt the Terrible Old Man about his long white hair and beard, or to break the small-paned windows of his dwelling with wicked missiles; but there are other things which frighten the older and more curious folk who sometimes steal up to the house to peer in through the dusty panes. These folk say that on a table in a bare room on the ground floor are many peculiar bottles, in each a small piece of lead suspended pendulum-wise from a string. And they say that the Terrible Old Man talks to these bottles, addressing them by such names as Jack, Scar-Face, Long Tom, Spanish Joe, Peters, and Mate Ellis, and that whenever he speaks to a bottle the little lead pendulum within makes certain definite vibrations as if in answer.`,
+        res:"https://americanliterature.com/author/h-p-lovecraft/short-story/the-terrible-old-man"
+    },
+    {
+        id:17,
+        lang:"en",
+        title:"The Selfish Giant",
+        text:`Every afternoon, as they were coming from school, the children used to go and play in the Giant's garden.
+
+        It was a large lovely garden, with soft green grass. Here and there over the grass stood beautiful flowers like stars, and there were twelve peach-trees that in the spring-time broke out into delicate blossoms of pink and pearl, and in the autumn bore rich fruit. The birds sat on the trees and sang so sweetly that the children used to stop their games in order to listen to them. "How happy we are here!" they cried to each other.
+        
+        One day the Giant came back. He had been to visit his friend the Cornish ogre, and had stayed with him for seven years. After the seven years were over he had said all that he had to say, for his conversation was limited, and he determined to return to his own castle. When he arrived he saw the children playing in the garden.`,
+        res:"https://americanliterature.com/author/oscar-wilde/short-story/the-selfish-giant"
+    },
+    {
+        id:18,
+        lang:"en",
+        title:"The Coming of the King",
+        text:`Some children were at play in their play-ground one
+        day, when a herald rode through the town, blowing a trumpet, and crying aloud, “The King! the King passes by this road to-day. Make ready for the King!”
+        
+        The children stopped their play, and looked at one another.
+        
+        “Did you hear that?” they said. “The King is coming. He may look over the wall and see our playground; who knows? We must put it in order.”
+        
+        The playground was sadly dirty, and in the corners were scraps of paper and broken toys, for these were careless children. But now, one brought a hoe, and another a rake, and a third ran to fetch the wheelbarrow from behind the garden gate. They labored hard, till at length all was clean and tidy.
+        
+        “Now it is clean!” they said; “but we must make it pretty, too, for kings are used to fine things; maybe he would not notice mere cleanness, for he may have it all the time.”
+        
+        Then one brought sweet rushes and strewed them on the ground; and others made garlands of oak leaves and pine tassels and hung them on the walls; and the littlest one pulled marigold buds and threw them all about the playground, “to look like gold,” he said.`,
+        res:"https://americanliterature.com/author/laura-e-richards/short-story/the-coming-of-the-king"
+    },
+    {
+        id:19,
+        lang:"en",
+        title:"The Man in the Moon",
+        text:`The Man in the Moon came tumbling down,
+        And enquired the way to Norwich;
+        He went by the south and burned his mouth
+        With eating cold pease porridge!
+        
+        What! Have you never heard the story of the Man in the Moon? Then I must surely tell it, for it is very amusing, and there is not a word of truth in it.
+        
+        The Man in the Moon was rather lonesome, and often he peeked over the edge of the moon and looked down upon the earth and envied all the people who lived together, for he thought it must be vastly more pleasant to have companions to talk to than to be shut up in a big planet all by himself, where he had to whistle to keep himself company.
+        
+        One day he looked down and saw an alderman sailing up through the air towards him. This alderman was being translated (instead of being transported, owing to a misprint in the law) and as he came near the Man in the Moon called to him and said,
+        
+        "How is everything down on the earth?"
+        
+        "Everything is lovely," replied the alderman, "and I would n't leave it if I was not obliged to."
+        
+        "What 's a good place to visit down there?" enquired the Man in the Moon.
+        
+        "Oh, Norwich is a mighty fine place," returned the alderman, "and it 's famous for its pease porridge;" and then he sailed out of sight and left the Man in the Moon to reflect upon what he had said.
+        
+        The words of the alderman made him more anxious than ever to visit the earth, and so he walked thoughtfully home, and put a few lumps of ice in the stove to keep him warm, and sat down to think how he should manage the trip.`,
+        res:"https://americanliterature.com/author/l-frank-baum/short-story/the-man-in-the-moon"
+    },
+    {
+        id:20,
+        lang:"en",
+        title:"The Last Leaf",
+        text:`In a little district west of Washington Square the streets have run crazy and broken themselves into small strips called "places." These "places" make strange angles and curves. One Street crosses itself a time or two. An artist once discovered a valuable possibility in this street. Suppose a collector with a bill for paints, paper and canvas should, in traversing this route, suddenly meet himself coming back, without a cent having been paid on account!
+
+        So, to quaint old Greenwich Village the art people soon came prowling, hunting for north windows and eighteenth-century gables and Dutch attics and low rents. Then they imported some pewter mugs and a chafing dish or two from Sixth Avenue, and became a "colony."
+        
+        At the top of a squatty, three-story brick Sue and Johnsy had their studio. "Johnsy" was familiar for Joanna. One was from Maine; the other from California. They had met at the table d'hte of an Eighth Street "Delmonico's," and found their tastes in art, chicory salad and bishop sleeves so congenial that the joint studio resulted.`,
+        res:"https://americanliterature.com/author/o-henry/short-story/the-last-leaf"
+    },
 ];
