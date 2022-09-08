273419aaba54bddd3f6dfe678711ece5b25a7f05
celalcok <celalcok@gmail.com>
2022-09-08 19:29:26 +0300

    data.js was edited

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export const texts = [
     {
         id:1,
         title:"Turkiye",
-        text:"Turkey or Türkiye, officially the Republic of Türkiye, is a transcontinental country located mainly on the Anatolian Peninsula in Western Asia, with a small portion on the Balkan Peninsula in Southeast Europe. It shares borders with the Black Sea to the north; Georgia to the northeast; Armenia, Azerbaijan, and Iran to the east; Iraq to the southeast; Syria and the Mediterranean Sea to the south; the Aegean Sea to the west; and Greece and Bulgaria to the northwest. Cyprus is located off the south coast. Turks form the vast majority of the nation's population and Kurds are the largest minority. Ankara is Turkey's capital, while Istanbul is its largest city and financial centre. ",
+        text:"Turkey or Türkiye, officially the Republic of Türkiye, is a transcontinental country located mainly on the Anatolian Peninsula in Western Asia, with a small portion on the Balkan Peninsula in Southeast Europe. It shares borders with the Black Sea to the north; Georgia to the northeast; Armenia, Azerbaijan, and Iran to the east; Iraq to the southeast; Syria and the Mediterranean Sea to the south; the Aegean Sea to the west; and Greece and Bulgaria to the northwest. Cyprus is located off the south coast. Ankara is Turkey's capital, while Istanbul is its largest city and financial centre. ",
         res:"https://en.wikipedia.org/wiki/Turkey"
     },
     {
